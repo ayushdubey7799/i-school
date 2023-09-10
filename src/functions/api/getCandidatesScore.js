8ff32808d43ed3a49f9d7740e436d3b2c77f5c88
innobraj <braj@innovatiview.com>
2023-09-10 23:44:35 +0530

    updating testing url

--- src/functions/api/getCandidatesScore.js
+++ src/functions/api/getCandidatesScore.js
@@ -8,8 +8,8 @@ export const getCandidatesScore = async (jobSummary) => {
  
 
     try {
-        //const response = await axios.post(`https://dev-api.intelliview.in/api/interviews/employers/candidateScores`,{jobSummaryHash:jobSummary});
-        const response = await axios.post(`http://localhost:7000/api/interviews/employers/candidateScores`,{jobSummaryHash:jobSummary});
+        const response = await axios.post(`https://dev-api.intelliview.in/api/interviews/employers/candidateScores`,{jobSummaryHash:jobSummary});
+        //const response = await axios.post(`http://localhost:7000/api/interviews/employers/candidateScores`,{jobSummaryHash:jobSummary});
        
         console.log('Data:', response.data);
         return response.data;
--- src/functions/api/getJds.js
+++ src/functions/api/getJds.js
@@ -7,8 +7,8 @@ export const getJds = async () => {
  
 
     try {
-        //const response = await axios.get(`https://dev-api.intelliview.in/api/interviews/employers/jds`);
-        const response = await axios.get(`http://localhost:7000/api/interviews/employers/jds`);
+        const response = await axios.get(`https://dev-api.intelliview.in/api/interviews/employers/jds`);
+       // const response = await axios.get(`http://localhost:7000/api/interviews/employers/jds`);
        
         console.log('Data:', response.data);
         return response.data;
