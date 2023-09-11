d349835055bf79c77cd0163fd8d8ec7a0da8a38a
Ayush <ayushdubey7799@gmail.com>
2023-09-11 11:26:26 +0530

    commit

--- src/functions/api/forget.js
+++ src/functions/api/forget.js
@@ -2,15 +2,7 @@ import axios from "axios";
 
 
 
-export const forget = async (id) => {
-  const token = JSON.parse(localStorage.getItem("token"));
-
-const config = {
-  headers: {
-    Authorization: `Bearer ${token}`,
-    'Content-Type': 'application/json'
-  }
-};
+export const forget = async () => {
 
     try {
         const response = await axios.get(`https://dev-api.intelliview.in/api/interviews/${id}/questions`,config);
