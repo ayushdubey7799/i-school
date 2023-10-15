62d4ff6c9f43b1a9177fa7315b3a39a69c91b9bf
Ayush <ayush.dubey@bonamisoftware.com>
2023-10-15 17:59:57 +0530

    employer-register api file added

---
+++ src/functions/api/employers/authentication/register.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+export const employerRegister = async (requestData) => {
+ 
+    try {
+        const response = await axios.post('https://dev-api.intelliview.in/api/onboards/register',requestData);
+        console.log('Data:', response.data);
+        return response.data;
+      } catch (error) {
+        console.error('Error:', error);
+      }
+}
