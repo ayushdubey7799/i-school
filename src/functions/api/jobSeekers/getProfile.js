62f68603fab533c16bd5c426418363c215b06245
abhi <pandeyabhi142002@gmail.com>
2023-12-29 17:52:04 +0530

    seeker profile API integrated for all available APIs

---
+++ src/functions/api/jobSeekers/getProfile.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+
+export const getProfile = async (profileId, accessToken) => {
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        const response = await axios.get(`https://dev-api.intelliview.in/api/profiles/${profileId}`, config);
+        console.log('Data:', response.data);
+        return response.data;
+    } catch (error) {
+        console.log('Error:', error);
+    }
+}
+
